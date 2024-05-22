bc6af3613e0d46f2dd74deecdf163b9e2c2a6f2e
Kamil Bąbaś <s38855@s.pwste.edu.pl>
2024-05-22 21:29:47 +0200

    create layout with slot

---
+++ resources/views/components/navbar.blade.php
@@ -0,0 +1,8 @@
+
+<link rel="stylesheet" href="{{asset('css/navbar.css')}}">
+
+<nav class="nav">
+        <span class="menu-buttons-container">
+            <a href="/" class="nav-button">Home</a>
+        </span>
+</nav>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+<x-layout>
+    <h1>test</h1>
+    <h2>to dziala</h2>
+</x-layout>
